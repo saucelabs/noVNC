1635e3e46f23595e74928c50384d27df185ae163
aluedeke <aluedeke@saucelabs.com>
2019-01-22 15:10:44 +0100

    fix import

--- core/index.js
+++ core/index.js
@@ -1,6 +1,6 @@
-import RFB from './rfb';
-import Keysym from './input/keysym';
-import * as Browser from './util/browser';
+import RFB from './rfb.js';
+import Keysym from './input/keysym.js';
+import * as Browser from './util/browser.js';
 
 export default RFB;
 export { RFB, Keysym, Browser };
